b3c703a4639af821747f572b5c96da9b6859763b
A-Abd <abrar.abdulwahed@gmail.com>
2023-09-09 23:09:41 +0300

    feat: async action

--- redux-toolkit/src/App.js
+++ redux-toolkit/src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchUsers } from './app/features/user/userSlice';
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [dispatch])
+
+  const contents = useSelector((state) => state.user.data)
+  const isLoading = useSelector((state) => state.user.loading)
+  const error = useSelector((state) => state.user.error)
+
+  if (isLoading) {
+    return 'loading...'
+  }
+
+  if (error) {
+    return error
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='grid gap-4 grid-cols-2  md:grid-cols-4 lg:grid-cols-8  p-4'>
+      {contents.map((content) => (
+        <div key={content.id}>
+          <p>{content.name}</p>
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
 export default App;
--- redux-toolkit/src/app/store.js
+++ redux-toolkit/src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cakeReducer, { order, reFund } from "./features/cake/cakeSlice";
 import iceCreamReducer from "./features/iceCream/iceCreamSlice";
+import userReducer, { fetchUsers } from "./features/user/userSlice";
 // import logging from "redux-logger";
 // const logger = logging.createLogger();
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: iceCreamReducer,
+        user: userReducer,
     },
     // middleware: (getDefaultMiddleware) => getDefaultMiddleware.concat(logger)
 })
@@ -16,8 +18,10 @@ const unsubscribe = store.subscribe(()=>{
     console.log('updated', store.getState());
 });
 
-store.dispatch(order());
-store.dispatch(order());
-store.dispatch(reFund(3));
+// store.dispatch(order());
+// store.dispatch(order());
+// store.dispatch(reFund(3));
+store.dispatch(fetchUsers());
+store.dispatch(fetchUsers());
 unsubscribe();
 export default store;
